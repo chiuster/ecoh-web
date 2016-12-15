1ed77063ba63eaad2919786a1946be6019130c06
aaka3207 <aakash2@uic.edu>
2016-12-15 10:51:23 -0600

    update with business section pending

---
+++ javascript/firebase.js
@@ -0,0 +1,22 @@
+/**
+ * Created by aaka3 on 12/12/2016.
+ */
+    // Set the configuration for your app
+    // TODO: Replace with your project's config object
+// Initialize Firebase
+var config = {
+        apiKey: "AIzaSyBRcPx7Mr9Qvm_IQ0NxYtiQW7TZUDQBnho",
+        authDomain: "project-64989311639038962.firebaseapp.com",
+        databaseURL: "https://project-64989311639038962.firebaseio.com",
+        storageBucket: "project-64989311639038962.appspot.com",
+        messagingSenderId: "1060550801740"
+    };
+
+fb = firebase.initializeApp(config);
+var db = firebase.database
+
+
+
+
+
+
